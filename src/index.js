2321a33fe02efb708a79ead8013d27590bfea706
panospd <anastapd@gmail.com>
2021-03-23 17:11:16 +0000

    Impelemented general asset type and modified file size threshold to 3 kbs. If more images will be treated as resource

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import addImage from "./add-image.js";
 import helloWorld from "./hello-world.js";
 
 helloWorld();
+addImage();
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,20 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "./dist"),
+    publicPath: "dist/",
   },
   mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 3 * 1024,
+          },
+        },
+      },
+    ],
+  },
 };
